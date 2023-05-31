b1cda3ea6951c995c5c626e6a30927a428c5e43c
Linus Arver <linusarver@gmail.com>
2023-05-31 20:14:45 +1000

    formatting

--- misc.js
+++ misc.js
@@ -3,7 +3,7 @@ $(function() {
         var $this = $(this);
         var href = $this.attr("id");
         $this.wrapInner(
-            $("<a/>").attr("href", '#' + href)
+            $("<a/>").attr("href", "#" + href)
                      .addClass("section-heading"));
     });
 });
@@ -19,52 +19,52 @@ function scrollIntoViewIfNeeded(target) {
 }
 
 function deactivate_other_toc_items(hash) {
-  $('#text-table-of-contents a').each((index, elt) => {
-    if (elt.hash !== hash) {
-      $(elt).removeClass('active');
-    }
-  })
+    $("#text-table-of-contents a").each((index, elt) => {
+        if (elt.hash !== hash) {
+            $(elt).removeClass("active");
+        }
+    })
 }
 
 function get_toc_item(hash) {
-  return $(`#text-table-of-contents a[href='${hash}']`)[0];
+    return $(`#text-table-of-contents a[href='${hash}']`)[0];
 }
 
 $(document).ready(() => {
-  $('#text-table-of-contents ul').first().addClass('nav');
-  $('#text-table-of-contents ul li').addClass('nav-item');
-  $('#text-table-of-contents ul li a').addClass('nav-link');
+    $("#text-table-of-contents ul").first().addClass("nav");
+    $("#text-table-of-contents ul li").addClass("nav-item");
+    $("#text-table-of-contents ul li a").addClass("nav-link");
 
-  $("#text-table-of-contents a").click((elt) => {
-    var tocItem = get_toc_item(elt.target.hash);
-    $(tocItem).addClass('active');
-    deactivate_other_toc_items(elt.target.hash);
-  });
+    $("#text-table-of-contents a").click((elt) => {
+        var tocItem = get_toc_item(elt.target.hash);
+        $(tocItem).addClass("active");
+        deactivate_other_toc_items(elt.target.hash);
+    });
 
-  $("*[id^='outline-container-h-']").each((index, elt) => {
-    var hash = elt.getAttribute('id')
-    hash = hash.replace('outline-container-', '#')
-    var tocItem = get_toc_item(hash);
-    elt.addEventListener('mouseover', () => {
-      $(tocItem).addClass("active");
-      deactivate_other_toc_items(hash);
+    $("*[id^='outline-container-h-']").each((index, elt) => {
+        var hash = elt.getAttribute("id")
+        hash = hash.replace("outline-container-", "#")
+        var tocItem = get_toc_item(hash);
+        elt.addEventListener("mouseover", () => {
+            $(tocItem).addClass("active");
+            deactivate_other_toc_items(hash);
+        });
+        elt.addEventListener("mouseover", (e) => {
+            // If we don't call stopPropagation(), we end up scrolling *all*
+            // elements in the stack, which means we will try to scroll e.g.,
+            // Section 5 and Section 5.1.2.3 (when we only want the latter).
+            e.stopPropagation();
+            // Unfortunately, scrollIntoViewIfNeeded is not supported on
+            // Firefox. Otherwise we could do
+            //
+            //    elems[0].scrollIntoViewIfNeeded({ block: "center" });
+            //
+            // instead. So here we call the custom function that does what we
+            // want.  See https://stackoverflow.com/a/37829643/437583.
+            scrollIntoViewIfNeeded(tocItem);
+        });
+        elt.addEventListener("mouseout", () => {
+            $(tocItem).removeClass("active");
+        });
     });
-    elt.addEventListener('mouseover', (e) => {
-      // If we don't call stopPropagation(), we end up scrolling *all*
-      // elements in the stack, which means we will try to scroll e.g.,
-      // Section 5 and Section 5.1.2.3 (when we only want the latter).
-      e.stopPropagation();
-      // Unfortunately, scrollIntoViewIfNeeded is not supported on Firefox.
-      // Otherwise we could do
-      //
-      //    elems[0].scrollIntoViewIfNeeded({ block: "center" });
-      //
-      // instead. So here we call the custom function that does what we want.
-      // See https://stackoverflow.com/a/37829643/437583.
-      scrollIntoViewIfNeeded(tocItem);
-    });
-    elt.addEventListener('mouseout', () => {
-      $(tocItem).removeClass("active");
-    });
-  });
 });
