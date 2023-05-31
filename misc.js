89c80eb226afaea575e3a9e14b17a5c5c09ffe0d
Linus Arver <linusarver@gmail.com>
2023-05-31 17:17:22 +1000

    toc: simplify scrollspy implementation
    We roll our own instead of using Bootstrap, because it's simpler.

--- misc.js
+++ misc.js
@@ -8,10 +8,6 @@ $(function() {
     });
 });
 
-function activate_toc_item_on_click (item){
-    $(item).toggleClass("active");
-}
-
 function scrollIntoViewIfNeeded(target) {
     if (target.getBoundingClientRect().bottom > window.innerHeight) {
         target.scrollIntoView(false);
@@ -22,24 +18,23 @@ function scrollIntoViewIfNeeded(target) {
     }
 }
 
-$(document).ready(function() {
+$(document).ready(() => {
   $('#text-table-of-contents ul').first().addClass('nav');
   $('#text-table-of-contents ul li').addClass('nav-item');
   $('#text-table-of-contents ul li a').addClass('nav-link');
 
-  const scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    smoothScroll: true,
-    target: '#text-table-of-contents'
-  })
-
-  $("#text-table-of-contents a").click(function() {
-    activate_toc_item_on_click(this);
-  });
-
-  const firstScrollSpyEl = document.querySelector('[data-bs-spy="scroll"]')
-  firstScrollSpyEl.addEventListener('activate.bs.scrollspy', () => {
-    let elems = document.getElementsByClassName('active');
-    if (elems.length > 0) {
+  $("*[id^='outline-container-h-']").each((index, elt) => {
+    var s = elt.getAttribute('id')
+    s = s.replace('outline-container-', '')
+    var tocItem = $(`#text-table-of-contents a[href='#${s}']`)[0];
+    elt.addEventListener('mouseover', () => {
+      $(tocItem).addClass("active");
+    });
+    elt.addEventListener('mouseover', (e) => {
+      // If we don't call stopPropagation(), we end up scrolling *all*
+      // elements in the stack, which means we will try to scroll e.g.,
+      // Section 5 and Section 5.1.2.3 (when we only want the latter).
+      e.stopPropagation();
       // Unfortunately, scrollIntoViewIfNeeded is not supported on Firefox.
       // Otherwise we could do
       //
@@ -47,7 +42,10 @@ $(document).ready(function() {
       //
       // instead. So here we call the custom function that does what we want.
       // See https://stackoverflow.com/a/37829643/437583.
-      scrollIntoViewIfNeeded(elems[0]);
-    }
-  })
+      scrollIntoViewIfNeeded(tocItem);
+    });
+    elt.addEventListener('mouseout', () => {
+      $(tocItem).removeClass("active");
+    });
+  });
 });
