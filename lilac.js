cd90cd27c80cd62d7884eb6c37b7a66bdbe4bb2e
Linus Arver <linusarver@gmail.com>
2023-07-16 15:05:00 -0700

    skip null condition
    If we're clicking on an intra-document link for the very first time,
    the history state is empty.

--- lilac.js
+++ lilac.js
@@ -90,6 +90,9 @@ $(document).ready(() => {
         // Save intra-document link into history, but only if it's not a repeat
         // of one already there.
         var hash = destination;
+        if (history.state === null) {
+            return;
+        }
         if (history.state.hash != hash) {
             history.pushState( //ref:HISTORY_PUSHSTATE
                 {hash: destination},
